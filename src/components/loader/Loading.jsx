600672a95adfdcfedf88b3ca53c3e4bee46ecc68
Pedro <pedrocastillo24.pc@gmail.com>
2022-09-04 18:20:50 -0300

    loading style

--- src/components/loader/Loading.jsx
+++ src/components/loader/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loading() {
   return (
-    <div className="mt-[25vh] text-white text-[10em] animate-pulse m-auto">
+    <div className="mt-[35vh] lg:mt-[25vh] text-white text-[5em]  animate-pulse m-auto">
       <span>LOADING...</span>
     </div>
   );
